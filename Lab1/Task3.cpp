c3b7b2e654433a413ff750e1ba36b89ba1cf6e9e
2017831031 <38094406+Dr-thrax@users.noreply.github.com>
2019-07-14 14:18:35 +0600

    h

--- Lab1/Task3.cpp
+++ Lab1/Task3.cpp
@@ -5,30 +5,37 @@ int main()
 {
     int m,k;
     vector <int> v;
+
     cout << "Enter the size of the array" << endl;
     int size,value;
     cin >> size;
-    cout << "Enter the array" << endl;
+    cout << "Enter the Array" << endl;
     for(int i=0;i<size;i++)
     {
         cin >> value;
         v.push_back(value);
     }
 
-    m = 157;
+    m = 256;
+    float A = 0.96;
 
-    vector <int> r(157);
-    for(int i=0;i<r.size();i++)
+    vector <int> r(m);
+
+    for(int i=0;i<m;i++)
+    {
         r[i] = -1;
+    }
 
     for(int i=0;i<v.size();i++)
     {
-        r[v[i]%m] = v[i];
+        int h;
+        h = floor(m*(v[i]*A - floor(v[i]*A)));
+        r[h] = v[i];
     }
-
-    cout << "h(k)\tk"<< endl;
+    cout << "h(k)" << "\t" << "k" << endl;
     for(int i=0;i<r.size();i++)
     {
+
         if(r[i]!=-1)
         {
             cout << i << "\t" << r[i] << endl;
